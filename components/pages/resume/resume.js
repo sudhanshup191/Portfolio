4451ec7ba11788377deb50494bbbddae9d7cca90
kirti7464 <71482336+kirti7464@users.noreply.github.com>
2024-05-04 00:26:37 +0530

    Delete components/pages/resume directory

--- components/pages/resume/resume.js
+++
@@ -1,9 +0,0 @@
-import { resumeLink } from "../../../util/constant";
-import styles from "./resume.module.css"
-export default function Resume (){
-    return (
-        <section id="resume" className={styles.wrapper}>
-            <iframe src={resumeLink} className={styles.iframeDiv} height={600} width={700}/>
-        </section>
-    )
-}
