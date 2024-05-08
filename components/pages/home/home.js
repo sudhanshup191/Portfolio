5cd55964aeebd1fe6b46104eb9eebada61477fb2
kirti7464 <71482336+kirti7464@users.noreply.github.com>
2024-05-09 00:45:32 +0530

    Update home.js

--- components/pages/home/home.js
+++ components/pages/home/home.js
@@ -7,9 +7,11 @@ import ArticleIcon from '@mui/icons-material/Article';
 export default function Home (){
     return (
         <section id="home" className={styles.wrap}>
-            <LinkedInIcon onClick={() => { window.open(contactDetails[contactDetails.length-1].value.linkedin, "_blank") }} className={styles.socialIcons} fontSize="large" />
-            <GitHubIcon onClick={() => { window.open(contactDetails[contactDetails.length-1].value.github, "_blank") }} className={styles.socialIcons} fontSize="large" />
-            <GoogleIcon onClick={() => { window.open(contactDetails[contactDetails.length-1].value.gmail, "_blank") }} className={styles.socialIcons} fontSize="large" />
+            <div className={styles.iconLayout}>
+            <LinkedInIcon onClick={() => { window.open(contactDetails[contactDetails.length - 1].value.linkedin, "_blank") }} className={styles.socialIcons} fontSize="large" />
+            <GitHubIcon onClick={() => { window.open(contactDetails[contactDetails.length - 1].value.github, "_blank") }} className={styles.socialIcons} fontSize="large" />
+            <GoogleIcon onClick={() => { window.open(contactDetails[contactDetails.length - 1].value.gmail, "_blank") }} className={styles.socialIcons} fontSize="large" />
+            </div>
             <div className={styles.outerResume }  onClick={()=>{window.open(resumeLink,"_blank")}}>
                <div className={styles.resume} >
                <ArticleIcon/> 
